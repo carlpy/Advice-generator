51698e29883003137fe9aaa2284523d47c3f94aa
Carlos Zambrano <carlosmariozambrano2015@gmail.com>
2023-02-07 13:00:00 -0500

    primera version de la pagina

---
+++ JS/app.js
@@ -0,0 +1,24 @@
+window.addEventListener('DOMContentLoaded', getAdvices)
+
+const ADVICE_API = 'https://api.adviceslip.com/advice';
+
+const adviceNumber = document.getElementById('advice-id');
+const adviceContainer = document.querySelector('.advice');
+const apiBtn = document.querySelector('.api-btn');
+
+async function getAdvices() {
+    try { 
+        const response = await fetch(ADVICE_API);
+        const data = await response.json();
+        const {id, advice} = data.slip;
+    
+        adviceNumber.innerText = id;
+        adviceContainer.innerText = advice;
+    } 
+    catch {
+        throw new Error('something went wrong: ' + response.status);
+    }
+}
+
+
+apiBtn.addEventListener('click', getAdvices)
